0cc9d3533c5c53d61e2ab45723740be721706749
eprogramming <00hf11@gmail.com>
2014-07-17 21:02:07 -0300

    initial structure.

---
+++ src/main/scala/org/eprogramming/scalaesh/ScalaeshApp.scala
@@ -0,0 +1,14 @@
+package org.esmerilprogramming.scalaesh
+
+import java.io.File
+import java.nio.file._
+
+/**
+* main app.
+* author helio frota at gmail dot com.
+*/
+object ScalaeshApp extends App {
+  println("--------------------")
+  println("initial structure...")
+  println("--------------------")
+}
