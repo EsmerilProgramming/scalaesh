796fddd9276f0c85a7d4c38efdc85c55e01afd64
eprogramming <00hf11@gmail.com>
2014-08-03 12:28:01 -0300

    compilation errors gone.

--- src/main/scala/org/eprogramming/scalaesh/ScalaeshApp.scala
+++ src/main/scala/org/eprogramming/scalaesh/ScalaeshApp.scala
@@ -13,6 +13,8 @@ import org.jboss.aesh.extensions.harlem.aesh._
 import org.jboss.aesh.extensions.pwd._
 import java.io._
 
+import org.jboss.aesh.terminal.TestTerminal
+
 
 /**
 * simple aesh scala wrapper.
@@ -22,9 +24,12 @@ object ScalaeshApp extends App {
 
   val pos = new PipedOutputStream();
   val pis = new PipedInputStream(pos);
+  val baos = new ByteArrayOutputStream();
+
   val sb = new SettingsBuilder()
- .inputStream(pis)
- .outputStream(new MtermPrintStream(textSpace, baos));
+  .terminal(new TestTerminal())
+  .inputStream(pis)
+  .outputStream(new PrintStream(baos))
 
   val registry = new AeshCommandRegistryBuilder()
   .command(new Harlem())
